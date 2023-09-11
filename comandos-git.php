44b3a7d3a9370124a18cb031d6dde2e653b8be75
aldajassofl <aldajassofl@ittepic.edu.mx>
2023-09-11 07:33:01 -0700

    correción en la conclusión

--- comandos-git.php
+++ comandos-git.php
@@ -581,10 +581,7 @@ include('includes/encabezado.php');
     </div>
     <div class="intro-texto">
         <p>
-            Puedo concluir que los comandos de Git nos sirven para tener una buena administración en nuestro repositorio
-            local, para así poder subirlo a nuestro repositorio en línea, así que mientras más organizados tengamos
-            nuestros commits, mejor será la gestión de estos y por ende será más fácil revertir cambios. Por eso son
-            importantes saber los comandos.
+        Puedo concluir que los comandos de Git nos sirven para tener una buena administración en nuestro repositorio local, para así poder subirlo a nuestro repositorio en línea, así que mientras más organizados tengamos nuestros commits, mejor será la gestión de estos y por ende será más fácil revertir cambios. Por eso son importantes saber los comandos.
         </p>
     </div>
 
